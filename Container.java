646b9061554c209c0482aeba6fb39b16d2937f8d
SHETTYSAMARTH30 <54606449+SHETTYSAMARTH30@users.noreply.github.com>
2020-04-02 11:58:13 -0700

    Create Container.java

---
+++ Container.java
@@ -0,0 +1,21 @@
+class Container {
+    public int maxArea(int[] height) {
+        
+        int left=0;
+        int right=height.length-1;
+        int maxArea=0;
+        
+        while(left < right){
+            
+            maxArea=Math.max(maxArea,Math.min(height[left],height[right])*(right-left));
+            
+            if(height[left] < height[right])
+                left+=1;
+            else
+                right-=1;
+        }
+        
+        return maxArea;
+        
+    }
+}
